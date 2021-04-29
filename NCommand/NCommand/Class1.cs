d05162df25e902b599bfcb2546a6eecddb4b6915
Denis Shcherbakov <sherbakov4599@gmail.com>
2021-04-29 14:41:01 +0300

    first commit

---
+++ NCommand/NCommand/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace NCommand
+{
+    class CustomCommands
+    {
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Запуск", "Launch", typeof(CustomCommands), myInputGestures);
+        }
+        public static RoutedUICommand Launch { get; }
+    }
+}
